529a812cea7ce488cd03beda1ccd91034e4e8191
Thanat <bztzn512@gmail.com>
2025-11-19 10:35:25 -0500

    addJWTmiddleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,7 +1,41 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { User } from "../models/Model";
+import { IUser } from "../models/type";
 
-interface jwtPayload {
-  id: string;
-  email: string;
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser | null;
+    }
+  }
 }
+
+export {};
+
+export const requireAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).json({ message: "Authorization token required" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const { _id } = jwt.verify(
+      token as string,
+      process.env.JWT_SECRET as string
+    ) as { _id: string };
+
+    req.user = await User.findOne({ _id }).select("_id");
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: "Request is not authorized" });
+  }
+};
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/authController";
+import { requireAuth } from "../middleware/authMiddleware";
 
 const router = Router();
+//require auth for workout
+/* router.use(requireAuth) */
 
 //POST /api/auth/register
 router.post("/register", registerUser);
 
 //POST /api/auth/login
-router.post("/login", loginUser);
+router.post("/login", requireAuth, loginUser);
 
 export default router;

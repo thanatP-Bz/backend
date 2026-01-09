0e0fa120c53e22e5d6236d4d4a6465a9d727512a
Thanat <bztzn512@gmail.com>
2026-01-09 11:30:49 -0500

    'cookie added'

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -44,17 +44,22 @@ export const loginController = asyncHandler(
   async (req: Request, res: Response) => {
     const result = await login(req.body);
 
-    res.cookie("refreshToken", result.refreshToken),
-      {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      };
+    res.cookie("accessToken", result.accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    res.cookie("refreshToken", result.refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
     res.status(200).json({
       message: result.message,
-      accessToken: result.accessToken,
       user: result.user,
     });
   }

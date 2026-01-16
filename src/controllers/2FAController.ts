f8b3e0a0e1d580c5cf7c2ca80d44d0dcb4914466
Thanat <bztzn512@gmail.com>
2026-01-15 22:04:20 -0500

    2FA

---
+++ src/controllers/2FAController.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../utils/asyncHandler";
+import {
+  enabled2FA,
+  disable2FA,
+  verify2FASetup,
+  regenerateBackupCodes,
+} from "../services/2FA.service";
+
+import { ApiError } from "../utils/ApiError";
+
+//enable 2FA
+export const enable2FAController = asyncHandler(async () => {
+  async (req: Request, res: Response) => {
+    const email = req.user?.email;
+
+    if (!email) {
+      throw new ApiError("Unathorized", 401);
+    }
+
+    const result = await enabled2FA(email);
+    return res.status(200).json(result);
+  };
+});
+
+//verify 2FA
+export const verify2FASetupController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const email = req.user?.email;
+    const { token } = req.body;
+
+    if (!email) {
+      throw new ApiError("Unauthorized", 401);
+    }
+
+    if (!token) {
+      throw new ApiError("token is required");
+    }
+
+    const result = await verify2FASetup(email, token);
+
+    return res.status(200).json(result);
+  }
+);
+
+//disable2FA
+export const disable2FAController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { email } = req.user?.email;
+    const { password } = req.body;
+
+    if (!email) {
+      throw new ApiError("Unauthorized", 401);
+    }
+
+    if (!password) {
+      throw new ApiError("Password is required", 401);
+    }
+
+    const result = await disable2FA(email, password);
+    return res.status(200).json(result);
+  }
+);
+
+//regenrate backup codes
+export const regenerateBackendCodesController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { email } = req.user?.email;
+
+    if (!email) {
+      throw new ApiError("Unthorized", 401);
+    }
+
+    const result = await regenerateBackupCodes(email);
+    return res.status(200).json(result);
+  }
+);
---
+++ src/routes/2FARoutes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  enable2FAController,
+  verify2FASetupController,
+  disable2FAController,
+  regenerateBackendCodesController,
+} from "../controllers/2FAController";
+import { requireAuth } from "../middleware/authMiddleware";
+
+const router = Router();
+
+router.use(requireAuth);
+
+//generate QR codes authetication
+router.post("/enable", enable2FAController);
+
+//Verify 2FA setup
+router.post("/verify-setup", verify2FASetupController);
+
+//disable 2FA - required password
+router.post("disable", disable2FAController);
+
+//Regenerate backup codes
+router.post("/regenerate-backup-codes", regenerateBackendCodesController);
+
+export default router;

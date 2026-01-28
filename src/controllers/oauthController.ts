99d195d199040b3f09ab4785996a9ab5d7855f90
Thanat <bztzn512@gmail.com>
2026-01-27 22:39:53 -0500

    updated

--- src/controllers/oauthController.ts
+++ src/controllers/oauthController.ts
@@ -36,7 +36,7 @@ export const googleCallbackController = async (req: Request, res: Response) => {
     );
 
     // ✅ NEW: Include sessionId in redirect URL
-    const redirectUrl = `${process.env.FRONTEND_URL}/oauth/callback?accessToken=${accessToken}&refreshToken=${refreshToken}&sessionId=${session._id.toString()}&user=${userData}`;
+    const redirectUrl = `${"https://mern-auth-frontend-rozs.onrender.com"}/oauth/callback?accessToken=${accessToken}&refreshToken=${refreshToken}&sessionId=${session._id.toString()}&user=${userData}`;
 
     res.redirect(redirectUrl);
   } catch (error) {

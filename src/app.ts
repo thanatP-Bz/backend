bbf0149dc321518cc1d4bbcf462196b7f8be2758
Thanat <bztzn512@gmail.com>
2026-04-28 17:42:16 -0400

    fix URL

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,12 @@ import { errorHandler, notFound } from "./middleware/errorHandler";
 
 const app: Application = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

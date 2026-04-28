2f6c1f59469487e60448dce5c6210223023cb692
Thanat <bztzn512@gmail.com>
2026-04-28 18:01:00 -0400

    fix dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
